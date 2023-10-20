b0de71bdb1dabb08d759ab08dbe868250619971e
dangtruong <dangductruong12b4.com>
2023-10-20 23:46:22 +0700

    third time

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
+import DefaultLayout  from "./components/Layout/DefaultLayout";
+import { publicRoutes } from "./routes";
+
 function App() {
-  return (
-    <div className="App">
-      <h1>React app</h1>
-    </div>
+  return (  
+     <Router>
+        <div className="App">
+          <Routes>
+           {publicRoutes.map((route,index)=>{
+            const Layout = route.layout || DefaultLayout
+            const Page =route.component
+            return <Route key={index} path={route.path} 
+            element={<Layout>
+              <Page/>
+            </Layout>}/>
+           })}
+
+          </Routes>
+        </div>
+     </Router>
+    
   );
 }
 
---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+import Home from '../pages/Home';
+import Following from '../pages/Following';
+import Profile from '../pages/Profile';
+const publicRoutes=[
+{path: '/', component: Home},
+{path: '/Following', component: Following},
+{path: '/Profile', component: Profile}
+]
+const privateRoutes=[
+    
+]
+export{publicRoutes,privateRoutes}
